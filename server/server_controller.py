9ee64f663ba84cd847468013c6698f138ec654fc
sapirani <sapirani@post.bgu.ac.il>
2025-10-03 18:41:27 +0300

    add all necessary routs

--- server/server_controller.py
+++ server/server_controller.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter
+
+from server.server_requests.all_ips_in_country_request import AllIPsInCountryRequest
 from server.server_requests.country_request import CountryRequest
 
 server_router = APIRouter()
@@ -6,3 +8,11 @@ server_router = APIRouter()
 @server_router.post("/get_country")
 async def get_ip_country(request: CountryRequest):
     return {"message": request.ip_address}
+
+@server_router.get("/get_all_ip_in_country")
+async def get_all_ips(request: AllIPsInCountryRequest):
+    return {"message": request.country}
+
+@server_router.get("/get_top_countries")
+async def get_top_countries():
+    return ["a", "b", "c", "d", "e"]
